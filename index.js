d3150bf5639768c6d44b5ec81c072eea775ecaed
OMKAR AGRAWAL <10913160+Omkaragrawal@users.noreply.github.com>
2024-06-19 16:39:42 +0530

    add get-data API

--- index.js
+++ index.js
@@ -14,13 +14,33 @@ app.get('/', (req, res, next) => {
   });
 
 app.get('/get-data', middleWareWrapper(async (req, res) => {
-    const TEMP_DATA = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const TEMP_DATA = await fetch('https://jsonplaceholder.typicode.com/albums');
 
-    const USER_DATA = await TEMP_DATA.json()
+    const ALBUM_DATA = await TEMP_DATA.json()
 
-    console.log(USER_DATA.length);
+    let filteredAlbumData = ALBUM_DATA.filter((album, index) => index < 2)
 
-    res.json(USER_DATA);
+    const PHOTOS_DATA = [];
+
+    for (let album of filteredAlbumData) {
+        const TEMP_PICTURE_DATA = await fetch(`https://jsonplaceholder.typicode.com/albums/${album.id}/photos`);
+
+        const PICTURES_DATA = await TEMP_PICTURE_DATA.json();
+
+        PHOTOS_DATA.push(
+            ...PICTURES_DATA.reduce((previousValue, currentValue, index) => {
+            if (index < 2) {
+                previousValue.push(currentValue);
+            }
+            return previousValue;
+        }, [])
+    );
+    }
+
+    res.json ({
+        albums: filteredAlbumData,
+        photos: PHOTOS_DATA,
+    });
 
 }));
 
